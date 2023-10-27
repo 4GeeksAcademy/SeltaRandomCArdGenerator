deb46d224da5d3db2ed594b2e4d8e124fb2b0141
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-10-27 20:03:56 +0000

    Missing JS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#card").classList.add(generateRandomSuits());
+  document.querySelector("#card").innerHTML = generateRandomNumber();
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumb = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumb];
+};
+
+let generateRandomSuits = () => {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
 };
