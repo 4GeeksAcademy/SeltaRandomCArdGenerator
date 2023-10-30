7cc96748ff568107eae837ddf6c64b14c067c707
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-10-30 18:35:48 +0000

    Fixed Js and added button and timer

--- src/app.js
+++ src/app.js
@@ -5,35 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  document.querySelector("#card").classList.add(generateRandomSuits());
-  document.querySelector("#card").innerHTML = generateRandomNumber();
-
+window.onload = function() {
   //write your code here
+  generateRandomCard();
 };
 
-let generateRandomNumber = () => {
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let indexNumb = Math.floor(Math.random() * numbers.length);
-  return numbers[indexNumb];
-};
-
-let generateRandomSuits = () => {
+function generateRandomCard() {
+  let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let suits = ["♦", "♥", "♠", "♣"];
+  let indexNumb = Math.floor(Math.random() * num.length);
   let indexSuits = Math.floor(Math.random() * suits.length);
-  return suits[indexSuits];
-};
+
+  // connecting with CSS
+  const cardDiv = document.querySelector(".card");
+  cardDiv.classList = "card " + suits[indexSuits];
+
+  const suitElement = suits[indexSuits];
+  const numElement = num[indexNumb];
+
+  const top = cardDiv.querySelector(".top-suit");
+  const bot = cardDiv.querySelector(".bot-suit");
+  const value = cardDiv.querySelector(".num");
+
+  top.textContent = suitElement;
+  bot.textContent = suitElement;
+  value.textContent = numElement;
+}
+const button = document.querySelector("button");
+button.addEventListener("click", function() {
+  generateRandomCard();
+});
+function tenSecondTimer() {
+  generateRandomCard();
+}
+setTimeout(tenSecondTimer, 10000);
