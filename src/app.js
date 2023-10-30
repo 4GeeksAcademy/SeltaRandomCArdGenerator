bc7abd62454c574942831a0a9aad20777213c073
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-10-30 19:33:02 +0000

    Missing func for height and width

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ const button = document.querySelector("button");
 button.addEventListener("click", function() {
   generateRandomCard();
 });
+
 function tenSecondTimer() {
   generateRandomCard();
 }
